54084747ae0f5de3502924b5bfa233710a6ccc13
omerEtzion <etzion.omer@gmail.com>
2022-11-14 12:36:35 +0200

    changed the function signatures to receive string

--- Engine3D/texture.h
+++ Engine3D/texture.h
@@ -8,7 +8,7 @@ class Texture
 public:
 	Texture(const std::string& fileName);
 	Texture(int width, int height,unsigned char *data);
-	Texture(const std::string& fileName, std::string effect, int halftone_parameter); //constructor of a texture with a specific effect base on an image
+	Texture(const std::string& fileName, std::string effect, unsigned int halftone_parameter); //constructor of a texture with a specific effect base on an image
 	void Bind(int slot);
 	inline int GetSlot(){return m_texture;}
 	 ~Texture();
@@ -17,9 +17,9 @@ private:
 	Texture(const Texture& texture) {}
 	void operator=(const Texture& texture) {}
 	unsigned int m_texture;
-	void edge_detection(unsigned char*, int);
-	void halftone(unsigned char*, int);
-	void floyd_steinberg(unsigned char*, int);
+	void edge_detection(const std::string& fileName, unsigned int halftone_parameter);
+	void halftone(const std::string& fileName, unsigned int halftone_parameter);
+	void floyd_steinberg(const std::string& fileName, unsigned int halftone_parameter);
 
 };
 
