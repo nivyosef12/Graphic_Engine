d5d09615bc3b66011b45916ac4ad86b753ea385e
nivyosef12 <nivyosef12@gmail.com>
2022-11-16 21:16:44 +0200

    implemented canny

--- Game/game.cpp
+++ Game/game.cpp
@@ -1,3 +1,5 @@
+// TODO list
+// 1. global vars
 #define _USE_MATH_DEFINES
 
 #include "game.h"
@@ -5,6 +7,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include "stb_image.h"
 #include <cmath>
+#include <map>
 
 static void printMat(const glm::mat4 mat)
 {
@@ -34,13 +37,15 @@ void Game::Init()
 	std::string fileName = "../res/textures/lena256.jpg";
 	int width, height, numComponents;
 	unsigned char* data = stbi_load((fileName).c_str(), &width, &height, &numComponents, 4); //extract data from the image
-	
-	unsigned char* new_data = edge_detection(width, height, data);
+	unsigned char* new_data = new unsigned char[4 * width * height]; //TODO: memory leaks!!
+
+	edge_detection(data, new_data, width, height);
 	AddTexture(width, height, new_data);
 	// AddTexture("../res/textures/lena256.jpg",false);
 	//AddTexture();
 
 	stbi_image_free(data);
+	delete[] new_data;
 	// AddTexture("../res/textures/lena256.jpg","edge_detection", 4);
 	// AddTexture("../res/textures/lena256.jpg",false);
 	// AddTexture("../res/textures/lena256.jpg", "edge_detection", 0);
@@ -56,7 +61,7 @@ void Game::Init()
 	//ReadPixel(); //uncomment when you are reading from the z-buffer
 }
 
-unsigned char* Game::edge_detection(int width, int height, unsigned char* data)
+void Game::edge_detection(unsigned char* data, unsigned char* new_data, int width, int height)
 {
 	unsigned char* smoothed_data = new unsigned char[4 * width * height]; //TODO: memory leaks!!
 	unsigned char* derived_data = new unsigned char[4 * width * height]; //TODO: memory leaks!!
@@ -66,11 +71,13 @@ unsigned char* Game::edge_detection(int width, int height, unsigned char* data)
 
 	smoothing(data, smoothed_data, width, height);
 	derivative(smoothed_data, derived_data, pixel_theta, width, height);
-	// non_max_suppression(derived_data, non_max_suppression_data, pixel_theta, width, height);
-	
+	non_max_suppression(derived_data, non_max_suppression_data, pixel_theta, width, height);
+	hysteresis(non_max_suppression_data, new_data, width, height, 2, 20);
 
-	// TODO free data
-	return derived_data;
+	delete[] smoothed_data;
+	delete[] derived_data;
+	delete[] pixel_theta;
+	delete[] non_max_suppression_data;
 }
 
 void Game::smoothing(unsigned char* data, unsigned char* new_data, int image_width, int image_height) {
@@ -90,15 +97,15 @@ void Game::smoothing(unsigned char* data, unsigned char* new_data, int image_wid
 
 		}
 	}
-
-	convolution(data, new_data, gaussian, data_size, image_width, image_height);  // smoothing with gaussian filter
+	std::vector<std::vector<float>>& gaussian_ref = gaussian;
+	convolution(data, new_data, gaussian_ref, data_size, image_width, image_height);  // smoothing with gaussian filter
 }
 
 void Game::derivative(unsigned char* data, unsigned char* new_data, float* pixel_theta, int image_width, int image_height) {
 
 	const int data_size = 4 * image_height * image_width;
-	unsigned char* new_data_gx = new unsigned char[data_size]; //TODO: memory leaks!!
-	unsigned char* new_data_gy = new unsigned char[data_size]; //TODO: memory leaks!!
+	unsigned char* new_data_gx = new unsigned char[data_size]; 
+	unsigned char* new_data_gy = new unsigned char[data_size];
 
 	/*std::vector<std::vector<float>> Gx{
 	{-1, 0, 1},
@@ -123,8 +130,11 @@ void Game::derivative(unsigned char* data, unsigned char* new_data, float* pixel
 	{0, -param, 0}
 	};
 
-	convolution(data, new_data_gx, Gx, data_size, image_width, image_height);
-	convolution(data, new_data_gy, Gy, data_size, image_width, image_height);
+	std::vector<std::vector<float>>& Gx_ref = Gx;
+	std::vector<std::vector<float>>& Gy_ref = Gy;
+
+	convolution(data, new_data_gx, Gx_ref, data_size, image_width, image_height);
+	convolution(data, new_data_gy, Gy_ref, data_size, image_width, image_height);
 
 	for (int i = 0; i < data_size; i++) {
 		new_data[i] = sqrt((pow(new_data_gx[i], 2) + pow(new_data_gy[i], 2)));
@@ -133,14 +143,173 @@ void Game::derivative(unsigned char* data, unsigned char* new_data, float* pixel
 		else
 			pixel_theta[i] = atan(new_data_gy[i] / new_data_gx[i]);
 	}
-	// TODO free gx, gy
+
+	delete[] new_data_gx;
+	delete[] new_data_gy;
+}
+
+void Game::non_max_suppression(unsigned char* data, unsigned char* new_data, float* pixel_theta, int image_width, int image_height){
+
+	const int data_size = 4 * image_height * image_width;
+	int index_of_neighbor_in_1d_array;
+
+	std::map <int, std::vector<int>> angle_step = {
+		{6, {-1, 1, 1, -1}},
+		{4, {0, -1, 0, 1}},
+		{2, {-1, -1, 1, 1}},
+		{0, {-1, 0, 1, 0}}, 
+		{8, {-1, 0, 1, 0}}
+	};
+
+	for (int i = 0; i < data_size; i++) {
+
+		// 4 is (R, G ,B, alpha)
+		int data_row_as_3d_mat = i / (4 * image_width);
+		int data_column_as_3d_mat = (i % (4 * image_width)) / 4;
+		int data_color_as_3d_mat = i % 4;
+
+		// convert theta into int represinting its range
+		float curr_theta = pixel_theta[i];
+		int index_in_angle_step = ((curr_theta + (M_PI / 2)) / (M_PI / 8) + 1);
+		index_in_angle_step -= (index_in_angle_step % 2);
+		std::vector<int> steps;
+		try {
+			steps = angle_step.at(index_in_angle_step);
+		}
+		catch (std::out_of_range) {
+			steps = {0, 0};
+			continue;
+		}
+
+		// Compare edge strength of current pixel with edge strength of pixel in positive and negative gradient directions
+		index_of_neighbor_in_1d_array = check_neighbor(data_row_as_3d_mat, data_column_as_3d_mat, data_color_as_3d_mat, steps[0], steps[1], image_width, image_height);
+		if (index_of_neighbor_in_1d_array != -1 && data[i] < data[index_of_neighbor_in_1d_array]) {
+			new_data[i] = 0;
+			continue;
+		}
+		index_of_neighbor_in_1d_array = check_neighbor(data_row_as_3d_mat, data_column_as_3d_mat, data_color_as_3d_mat, steps[2], steps[3], image_width, image_height);
+		if (index_of_neighbor_in_1d_array != -1 && data[i] < data[index_of_neighbor_in_1d_array]) {
+			new_data[i] = 0;
+			continue;
+		}
+		// preserve value
+		new_data[i] = data[i];
+
+	}
+}
+
+void Game::hysteresis(unsigned char* data, unsigned char* new_data, int image_width, int image_height, double low_threshold, double high_threshold) {
+	
+	const int data_size = 4 * image_height * image_width;
+	unsigned char* top_left_to_bottom_right = new unsigned char[data_size]; 
+	unsigned char* bottom_right_to_top_left = new unsigned char[data_size];
+	unsigned char* top_right_to_bottom_left = new unsigned char[data_size];
+	unsigned char* bottom_left_to_top_right = new unsigned char[data_size];
+
+
+	std::vector<std::vector<int>>neighbors{
+	{-1, 1},  // right up
+	{1, -1},  // left down
+	{0, -1},  // left
+	{0, 1},   // right
+	{-1, -1}, // left up
+	{1, 1},   // right down
+	{-1, 0},  // up
+	{1, 0},   // down
+
+	};
+	
+	std::vector<std::vector<int>>&neighbors_ref = neighbors;
+
+	for (int i = 0; i < data_size; i++) {
+		// top_left_to_bottom_right
+		thresholding_pixel(data, top_left_to_bottom_right, neighbors_ref, i, image_width, image_height, low_threshold, high_threshold);
+
+		// bottom_right_to_top_left
+		thresholding_pixel(data, bottom_right_to_top_left, neighbors_ref, (data_size - 1) - i, image_width, image_height, low_threshold, high_threshold);
+	}	
+
+	int row = 0;
+	while (row < image_height) {
+		for (int column = (image_width * 4) - 1; column >= 0; column--) {
+			// index in 1d array
+			int index = row * image_width * 4 + column;
+
+			// top_right_to_bottom_left
+			thresholding_pixel(data, top_right_to_bottom_left, neighbors_ref, index, image_width, image_height, low_threshold, high_threshold);
+
+			// bottom_left_to_top_right
+			thresholding_pixel(data, bottom_left_to_top_right, neighbors_ref, (data_size - 1) - index, image_width, image_height, low_threshold, high_threshold);
+		}
+
+		row += 1;
+	}
+
+	for (int i = 0; i < data_size; i++) {
+		new_data[i] = top_left_to_bottom_right[i] + bottom_right_to_top_left[i] + top_right_to_bottom_left[i] + bottom_left_to_top_right[i];
+	}
+
+	delete[] top_left_to_bottom_right;
+	delete[] bottom_right_to_top_left;
+	delete[] top_right_to_bottom_left;
+	delete[] bottom_left_to_top_right;
+
+}
+
+void Game::thresholding_pixel(unsigned char* data, unsigned char* new_data, std::vector<std::vector<int>>& neighbors, int index, int width, int height, double low_threshold, double high_threshold) {
+	const int is_edge = 255;
+	const int is_not_edge = 0;
+	const int data_size = 4 * height * width;
+
+	int data_row_as_3d_mat;
+	int data_column_as_3d_mat;
+	int data_color_as_3d_mat;
+	int index_of_neighbor_in_1d_array;
+	bool weak_to_strong = false;
+
+
+	// strong pixel
+	if (data[index] > high_threshold) {
+		new_data[index] = is_edge;
+		return;
+	}
+
+	// unrelevant pixel
+	else if (data[index] < low_threshold) {
+		// do nothing ??
+		new_data[index] = is_not_edge;
+		return;
+	}
+
+	// weak pixel
+	// 4 is (R, G ,B, alpha)
+	data_row_as_3d_mat = index / (4 * width);
+	data_column_as_3d_mat = (index % (4 * width)) / 4;
+	data_color_as_3d_mat = index % 4;
+	weak_to_strong = false;
+
+	for (std::vector<int> neigbor : neighbors) {
+		index_of_neighbor_in_1d_array = check_neighbor(data_row_as_3d_mat, data_column_as_3d_mat, data_color_as_3d_mat, neigbor[0], neigbor[1], width, height);
+		if (index_of_neighbor_in_1d_array != -1 && data[index_of_neighbor_in_1d_array]  == 255) {
+			new_data[index] = is_edge;
+			weak_to_strong = true;
+			return;
+		}
+	}
+
+	// not an edge
+	if (!weak_to_strong)
+		new_data[index] = is_not_edge;
 }
 
 
-void Game::convolution(unsigned char* data, unsigned char* new_data, std::vector<std::vector<float>> gaussian_kernel, int data_size, int image_width, int image_height)
+void Game::convolution(unsigned char* data, unsigned char* new_data, std::vector<std::vector<float>>& kernel, int data_size, int image_width, int image_height)
 {
-	const int kernel_size = gaussian_kernel.size();
+	const int kernel_size = kernel.size();
 	const int middle = (kernel_size - 1) / 2;
+
+	// generate matrix (in size of kernel) to be multiplied by kernel
+	std::vector<std::vector<float>> matrix(kernel_size, std::vector<float>(kernel_size, 0));
 
 	for (int data_i = 0; data_i < data_size; data_i++) {
 
@@ -149,8 +318,6 @@ void Game::convolution(unsigned char* data, unsigned char* new_data, std::vector
 		int data_column_as_3d_mat = (data_i % (4 * image_width)) / 4;
 		int data_color_as_3d_mat = data_i % 4;
 
-		// generate matrix (in size of gaussian_kernel) to be multiplied by gaussian_kernel
-		std::vector<std::vector<float>> matrix(kernel_size, std::vector<float>(kernel_size, 0));
 		for (int matrix_i = 0; matrix_i < kernel_size; matrix_i++) {
 
 			int row = data_row_as_3d_mat - middle + matrix_i;
@@ -162,11 +329,11 @@ void Game::convolution(unsigned char* data, unsigned char* new_data, std::vector
 			}
 		}
 
-		// multiply matrix[x][y] with gaussian_kernel[x][y] and sum up to "new pixel"
+		// multiply matrix[x][y] with kernel[x][y] and sum up to "new pixel"
 		float new_pixel = 0;
 		for (int matrix_i = 0; matrix_i < kernel_size; matrix_i++) {
 			for (int matrix_j = 0; matrix_j < kernel_size; matrix_j++) {
-				new_pixel += matrix[matrix_i][matrix_j] * gaussian_kernel[matrix_i][matrix_j];
+				new_pixel += matrix[matrix_i][matrix_j] * kernel[matrix_i][matrix_j];
 			}
 		}
 
@@ -175,116 +342,18 @@ void Game::convolution(unsigned char* data, unsigned char* new_data, std::vector
 	}
 }
 
-void Game::non_max_suppression(unsigned char* data, unsigned char* new_data, float* pixel_theta, int image_width, int image_height){
-	const int data_size = 4 * image_height * image_width;
-	int neighbor_x;
-	int neighbor_y;
-	int neighbor_pixel;
-	int index_of_neighbor_in_1d_array;
-	for (int i = 0; i < data_size; i++) {
+int Game::check_neighbor(int row, int column, int pixel, int up_down, int left_right, int width, int height) {
+	int new_row = row + up_down;
+	int new_column = column + left_right;
+	int new_pixel = pixel;
+	int neighbor_index = new_row * width * 4 + new_column * 4 + new_pixel;
 
-		// 4 is (R, G ,B, alpha)
-		int data_row_as_3d_mat = i / (4 * image_width);
-		int data_column_as_3d_mat = (i % (4 * image_width)) / 4;
-		int data_color_as_3d_mat = i % 4;
-
-		float curr_theta = pixel_theta[i];
-
-		if (curr_theta <= 3 * M_PI / 8 && curr_theta >= M_PI / 8) {
-			// right up
-			neighbor_x = data_row_as_3d_mat - 1;
-			neighbor_y = data_column_as_3d_mat + 1;
-			neighbor_pixel = data_color_as_3d_mat;
-			index_of_neighbor_in_1d_array = neighbor_x * image_width * 4 + neighbor_y * 4 + neighbor_pixel;
-			if (neighbor_x >= 0 && neighbor_y <= image_height && data[i] < data[index_of_neighbor_in_1d_array]) {
-				new_data[i] = 0;
-				continue;
-			}
-
-			// left down
-			neighbor_x = data_row_as_3d_mat + 1;
-			neighbor_y = data_column_as_3d_mat - 1;
-			neighbor_pixel = data_color_as_3d_mat;
-			index_of_neighbor_in_1d_array = neighbor_x * image_width * 4 + neighbor_y * 4 + neighbor_pixel;
-			if (neighbor_x < image_height && neighbor_y >= 0 && data[i] < data[index_of_neighbor_in_1d_array]) {
-				new_data[i] = 0;
-				continue;
-			}
-
-		}
-		else if (curr_theta <= M_PI / 8 && curr_theta >=  -M_PI / 8) {
-			// left neighbor
-			neighbor_x = data_row_as_3d_mat;
-			neighbor_y = data_column_as_3d_mat - 1;
-			neighbor_pixel = data_color_as_3d_mat;
-			index_of_neighbor_in_1d_array = neighbor_x * image_width * 4 + neighbor_y * 4 + neighbor_pixel;
-			if (neighbor_y >= 0 && data[i] < data[index_of_neighbor_in_1d_array]) {
-				new_data[i] = 0;
-				continue;
-			}
-
-			// right neighbor
-			neighbor_x = data_row_as_3d_mat;
-			neighbor_y = data_column_as_3d_mat + 1;
-			neighbor_pixel = data_color_as_3d_mat;
-			index_of_neighbor_in_1d_array = neighbor_x * image_width * 4 + neighbor_y * 4 + neighbor_pixel;
-			if (neighbor_y < image_width && data[i] < data[index_of_neighbor_in_1d_array]) {
-				new_data[i] = 0;
-				continue;
-			}
-
-		}
-		else if (curr_theta <= -M_PI / 8 && curr_theta >= -3 * M_PI / 8) {
-			// left up
-			neighbor_x = data_row_as_3d_mat - 1;
-			neighbor_y = data_column_as_3d_mat - 1;
-			neighbor_pixel = data_color_as_3d_mat;
-			index_of_neighbor_in_1d_array = neighbor_x * image_width * 4 + neighbor_y * 4 + neighbor_pixel;
-			if (neighbor_y >= 0 && neighbor_x >= 0 && data[i] < data[index_of_neighbor_in_1d_array]) {
-				new_data[i] = 0;
-				continue;
-			}
-
-			// right down
-			neighbor_x = data_row_as_3d_mat + 1;
-			neighbor_y = data_column_as_3d_mat + 1;
-			neighbor_pixel = data_color_as_3d_mat;
-			index_of_neighbor_in_1d_array = neighbor_x * image_width * 4 + neighbor_y * 4 + neighbor_pixel;
-			if (neighbor_y < image_width && neighbor_x < image_height && data[i] < data[index_of_neighbor_in_1d_array]) {
-				new_data[i] = 0;
-				continue;
-			}
-
-		}
-		else {
-
-			// up neighbor
-			neighbor_x = data_row_as_3d_mat - 1;
-			neighbor_y = data_column_as_3d_mat;
-			neighbor_pixel = data_color_as_3d_mat;
-			index_of_neighbor_in_1d_array = neighbor_x * image_width * 4 + neighbor_y * 4 + neighbor_pixel;
-			if (neighbor_x >= 0 && data[i] < data[index_of_neighbor_in_1d_array]) {
-				new_data[i] = 0;
-				continue;
-			}
-
-			// down neighbor
-			neighbor_x = data_row_as_3d_mat + 1;
-			neighbor_y = data_column_as_3d_mat;
-			neighbor_pixel = data_color_as_3d_mat;
-			index_of_neighbor_in_1d_array = neighbor_x * image_width * 4 + neighbor_y * 4 + neighbor_pixel;
-			if (neighbor_x < image_height && data[i] < data[index_of_neighbor_in_1d_array]) {
-				new_data[i] = 0;
-				continue;
-			}
-		}
-
-		// preserve value
-		new_data[i] = data[i];
-
+	if ((0 <= new_row && new_row < height) && (0 <= new_column && new_column < width)) {
+		return neighbor_index;
 	}
-}
 
+	return -1;
+}
 
 void Game::halftone_pixel(unsigned char* data, unsigned char* new_data, int pixel_num, int width, std::vector<std::vector<unsigned char>> halftone_options)
 {
